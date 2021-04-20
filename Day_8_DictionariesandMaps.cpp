3b14cc6a77ea35bd2ccfc1852914caace9e87ee9
Mayur Bhamare <mayurbhamare2001@gmail.com>
2021-04-20 09:53:37 +0530

    Create Day_8_DictionariesandMaps.cpp

---
+++ Day_8_DictionariesandMaps.cpp
@@ -0,0 +1,32 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <map>
+using namespace std;
+
+
+int main() {
+    string name,number,key;
+    int n;
+    cin>>n;
+    map<string,string>phonebook;
+    for (int i=0; i<n; i++) 
+    {
+      cin>>name>>number;
+      phonebook[name]=number;
+    }
+    while(cin>>key)
+    {
+        if(phonebook.find(key)!=phonebook.end())
+        {
+            cout<<key<<"="<<phonebook.find(key)->second<<endl;
+        }
+        else
+        {
+            cout<<"Not found"<<endl;
+        }
+    }
+    return 0;
+}
